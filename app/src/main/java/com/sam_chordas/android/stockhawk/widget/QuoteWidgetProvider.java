df6150e436e45250bd46387faa15bbac45ae8c56
seventhmoon <funglam@7thmoon.com>
2016-04-25 21:26:03 +0800

    Added - Widget

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -1,12 +1,10 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
@@ -20,30 +18,25 @@ import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
 public class QuoteWidgetProvider extends AppWidgetProvider {
 
     void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                         int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_collection);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
-        // Instruct the widget manager to update the widget
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
 
 
-        // Create intent to launch MainActivity
         Intent intent = new Intent(context, MyStocksActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
-        // Set up collection
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            setRemoteAdapter(context, views);
-        } else {
-            setRemoteAdapterV11(context, views);
-        }
 
-        // Set up collection items
+        setRemoteAdapter(context, views);
+
+
         Intent clickIntentTemplate = new Intent(context, MyStocksActivity.class);
         PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
                 .addNextIntentWithParentStack(clickIntentTemplate)
@@ -69,28 +62,18 @@ public class QuoteWidgetProvider extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
     /**
      * Sets the remote adapter used to fill in the list items
      *
      * @param context the context used to launch the intent
-     * @param views RemoteViews to set the RemoteAdapter
+     * @param views   RemoteViews to set the RemoteAdapter
      */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
         views.setRemoteAdapter(R.id.widget_list,
                 new Intent(context, QuoteWidgetRemoteViewsService.class));
     }
 
-    /**
-     * Sets the remote adapter used to fill in the list items
-     *
-     * @param context the context to launch the intent
-     * @param views RemoteViews to set the RemoteAdapter
-     */
-    @SuppressWarnings("deprecation")
-    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
-        views.setRemoteAdapter(0, R.id.widget_list,
-                new Intent(context, QuoteWidgetRemoteViewsService.class));
-    }
+
 }
 
